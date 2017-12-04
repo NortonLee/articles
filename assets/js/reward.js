516cb852841011cb44cd39c5b0d2d65b5c20a4a2
NortonLee <lizard@yeah.net>
2017-12-04 14:04:11 +0800

    上线打赏功能

---
+++ assets/js/reward.js
@@ -0,0 +1,41 @@
+jQuery(document).ready(function() {
+	var QRBox	=	$('#QRBox');
+	var MainBox	=	$('#MainBox');
+	var AliPayQR	=	'/articles/assets/images/AliPayQR.png';
+	var WeChanQR	=	'/articles/assets/images/WeChatQR.png';
+
+	
+
+	function showQR(QR) {
+		if (QR) {
+			MainBox.css('background-image','url('+QR+')');
+		}
+		$('#DonateText,#donateBox,#github,.reward-text').addClass('blur');
+		QRBox.fadeIn(300,function(argument) {
+			MainBox.addClass('showQR');
+		});
+	}
+
+	$('#donateBox>li').click(function(event) {
+		var thisID	=	$(this).attr('id');
+		if (thisID === 'BTC') {
+			showQR(BTCQR);
+			// new Clipboard('#BTCBn');
+		} else if (thisID === 'AliPay') {
+			showQR(AliPayQR);
+		} else if (thisID === 'WeChat') {
+			showQR(WeChanQR);
+		}
+	});
+
+	MainBox.click(function(event) {
+		MainBox.removeClass('showQR').addClass('hideQR');
+		setTimeout (function(a) {
+			QRBox.fadeOut(300,function(argument) {
+				MainBox.removeClass('hideQR');
+			});
+			$('#DonateText,#donateBox,#github,.reward-text').removeClass('blur');
+		},600);
+
+	});
+});
